aa7d80400177176328d080145bf231b87a2fd785
Tahara Begum <tahara.b3gum123@gmail.com>
2013-07-09 16:50:58 -0400

    End of Tuesday, Need to fix my code

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,16 @@ require 'sinatra/reloader'
 number = (1 + rand(99)).to_s
 
 get "/" do
-	erb :index, :locals => {:number => number}
+	 params["guess"]
+	 erb :index, :locals => {:number => number}
 end
+
+def check_guess(guess)
+	if guess.to_i == number.to_i + 40
+		puts "TOO HIGH!"
+	elsif guess.to_i == number.to_i - 40
+		puts "TOO LOW"
+	end
+end
+
+
